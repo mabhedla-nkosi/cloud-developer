a4f6da7f89a586c9e90247fadf90d9583901f6d4
mabhedla-nkosi <nkosi.mlambo@gmail.com>
2022-06-14 13:23:56 +0200

    server file updates

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -1,6 +1,7 @@
-import express from 'express';
-import bodyParser from 'body-parser';
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
+import express, { Router, Request, Response } from 'express';
+import bodyParser from 'body-parser';
+//import { requireAuth } from '/exercises/udacity-c2-restapi/src/controllers/v0/users/routes/auth.router';
 
 (async () => {
 
@@ -28,8 +29,42 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
 
   /**************************************************************************** */
+  app.get( "/filteredimage", ( req: Request, res: Response ) => {
+    let imgurl=req.query.image_url;
+    if(imgurl===""){
+      res.status(404).send("Image url not found!");
+    }
+    //The image validation code idea was obtained from https://stackoverflow.com/questions/9714525/javascript-image-url-verify
+    let imgpattern=/^http.*\.(jpeg|jpg)$/g;
+    let checkimage=imgpattern.test(imgurl);
+    if(!checkimage){
+      res.status(404).send("Image was not found! Please put a correct url with an image.");
+    }
+    //The filterImage function idea was obtained from the generosity of https://github.com/Adewale-D-A/Udagram-Image-Filter-Project-2/blob/master/src/server.ts
+    async function filterImage(){
+      try{
+        let filtimg= await filterImageFromURL(imgurl);
+      //The send file function code idea was obtained from the generosity of https://github.com/Adewale-D-A/Udagram-Image-Filter-Project-2/blob/master/src/server.ts
+      return res.status(200).sendFile(filtimg, ()=>{
+        deleteLocalFiles([filtimg]);
+      });
+      }catch(ex){
+        //The 422 status code idea was obtained from the generosity of https://github.com/Adewale-D-A/Udagram-Image-Filter-Project-2/blob/master/src/server.ts
+        return res.status(422).send("The server can't process your image, sorry.")
+      }
+      
+    }
+    filterImage();
 
+  } );
   //! END @TODO1
+
+  //@TODO
+//Add an endpoint to GET a specific resource by Primary Key
+
+       // res.status(200).json({project});
+  
+
   
   // Root Endpoint
   // Displays a simple message to the user
